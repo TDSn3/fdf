0f413edd90af7ff649a256e92e96dfa836837e60
Thomas DA SILVA <tda-silv@e3r12p17.clusters.42paris.fr>
2022-10-26 15:21:34 +0200

    - push 8 - start gnl

--- source/fdf.c
+++ source/fdf.c
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:27:35 by tda-silv          #+#    #+#             */
-/*   Updated: 2022/10/26 13:59:06 by tda-silv         ###   ########.fr       */
+/*   Updated: 2022/10/26 14:35:19 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,16 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, L, H, "test");
 	img.img = mlx_new_image(mlx, L, H);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+	int			fd;
+	char		*str;
+	const char	*pathname = "mlx_util/maps/test_maps/plat.fdf";
+
+	fd = open(pathname, O_RDWR);
+	str = get_next_line(fd);
+	ft_printf("|%s", str);
+	free(str);
+	close(fd);
 
 	rotate_all(1480, 920, 30, mlx, mlx_win, &img);
 
