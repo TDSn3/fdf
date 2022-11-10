147bd3c50ab395dcc4729f213115954435452939
Thomas Da Silva <thomas@MacBook-Pro-de-Thomas.local>
2022-11-10 11:17:29 +0100

    gradient next 2

--- source/nb_nb_point.c
+++ source/nb_nb_point.c
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:39:07 by tda-silv          #+#    #+#             */
-/*   Updated: 2022/11/10 10:55:08 by tda-silv         ###   ########.fr       */
+/*   Updated: 2022/11/10 11:16:25 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,53 @@ t_li *nb_nb_point(t_data_util *d)
 		i++;
 	}
 
+//	int	i;
+	int pos;
 	int	list_size;
+	t_li	*cpy_one;
+	t_li	*cpy_two;
 
+	i = 0;
+	pos = 0;
 	list_size = li_size(li);
+	cpy_one = li;
 	printf("size li = %d\n", list_size);
+
+	while (cpy_one)
+	{
+		pos = cpy_one->content_one;
+		cpy_two = li;
+		while (cpy_two)
+		{
+			if (cpy_two->content_one > pos)
+				i++;
+			cpy_two = cpy_two->next;
+		}
+		cpy_one->content_two = i;
+		i = 0;
+		cpy_one = cpy_one->next;
+	}
+
+	for (t_li *cpy_three = li; cpy_three; cpy_three = cpy_three->next)
+	{
+		printf("%d %d\n", cpy_three->content_one, cpy_three->content_two);
+	}
+
+
+
+	t_li	*cpy_four;
+
+	cpy_four = li;
+	while (cpy_four)
+	{
+		cpy_four->content_three = (list_size / 255) * cpy_four->content_two;
+		cpy_four = cpy_four->next;
+	}
+
+	for (t_li *cpy_three = li; cpy_three; cpy_three = cpy_three->next)
+	{
+		printf("%d\t%d\t%d\n", cpy_three->content_one, cpy_three->content_two, cpy_three->content_three);
+	}
 
 	return (li);
 }
